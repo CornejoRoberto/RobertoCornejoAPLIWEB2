1dca2e530702cea6d6fd879918f11929c4205fcf
CornejoRoberto <roberto_Samp@outlook.com>
2023-06-21 23:56:50 -0500

    Ejercicio Docker
    Realizado en clase

---
+++ APLIWEB2-Ejercicio6/controllers/index.js
@@ -0,0 +1,10 @@
+const Product =  require('./product');
+const Category =  require('./category');
+const Receta = require('./receta');
+
+module.exports= {
+    Product,
+    Receta,
+    Category
+   
+}
---
+++ APLIWEB2-Ejercicio6/models/index.js
@@ -0,0 +1,11 @@
+const Product =  require('./product');
+const Category =  require('./category');
+const Receta = require('./receta');
+
+
+module.exports ={
+    Product,
+    Receta,
+    Category
+
+}
